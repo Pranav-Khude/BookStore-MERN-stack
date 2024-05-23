fdc681143a767e212d95ad430141302f5e23e67f
Pranav-Khude <202151077@iiitvadodara.ac.in>
2024-05-23 21:38:01 +0530

    MongoDB connected

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
-import {PORT} from './config.js';
+import mongoose from 'mongoose';
+import { PORT, mongoDBURL } from './config.js';
 
 const app = express();
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(200).send("Welcome To Book Store");
 });
+
+mongoose.connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error.message);
+    });
